d93fe3ed9869afa665e02636f1ee87f126ebcf65
ggzhang <z_root@126.com>
2021-05-06 23:40:32 +0800

    配置electron、antd、less

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button } from "antd";
+import logo from "./logo.svg";
+import "./App.less";
 
 function App() {
   return (
     <div className="App">
+      <Button type="primary">click</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
